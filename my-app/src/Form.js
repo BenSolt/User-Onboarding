65b121153be0becc00f5144e3b91ba1e8b339620
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-08-22 16:04:20 -0400

    fleshing out the form.js. adding: name, email, pasword, terms service, button

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -14,25 +14,29 @@ const NewUserForm = ({ errors, touched, values, status }) => {
  
     return (
         <div className="animal-form">
-          <h1>Animal Form</h1>
+          <h1>User Form</h1>
           <Form>
-            <Field type="text" name="species" placeholder="Species" />
-            {touched.species && errors.species && (
-              <p className="error">{errors.species}</p>
+            <Field type="text" name="userName" placeholder="Enter Name" />
+            {touched.userName && errors.userName && (
+              <p className="error">{errors.userName}</p>
             )}
     
-            <Field type="text" name="size" placeholder="Size" />
-            {touched.size && errors.size && <p className="error">{errors.size}</p>}
+            <Field type="text" name="Email" placeholder="Enter Email" />
+            {touched.email && errors.email && <p className="error">{errors.email}</p>}
+
+            <Field type="text" name="Password" placeholder="Enter Password" />
+            {touched.password && errors.password && <p className="error">{errors.password}</p>}
+            
     
-            <Field component="select" className="food-select" name="food">
+            {/* <Field component="select" className="food-select" name="food">
               <option>Please Choose an Option</option>
               <option value="herbivore">Herbivore</option>
               <option value="carnivore">Carnivore</option>
               <option value="omnivore">Omnivore</option>
-            </Field>
+            </Field> */}
     
             <label className="checkbox-container">
-              Vaccinations
+              Terms of Service
               <Field
                 type="checkbox"
                 name="vaccinations"
@@ -54,13 +58,45 @@ const NewUserForm = ({ errors, touched, values, status }) => {
             <button type="submit">Submit!</button>
           </Form>
     
-          {animals.map(animal => (
-            <ul key={animal.id}>
-              <li>Species: {animal.species}</li>
-              <li>Size: {animal.size}</li>
-              <li>Food: {animal.food}</li>
+          {animals.map(user => (
+            <ul key={user.id}>
+              <li>Name: {user.name}</li>
+              <li>Email: {user.email}</li>
+              <li>Password: {user.password}</li>
             </ul>
           ))}
         </div>
       );
 };
+
+const FormikUserForm = withFormik({
+    // object destructuring. We could do values.species but we are destructuring it so we can just put species. You see the same thing in Props a lot so in stead of props.values you would see {values}
+    mapPropsToValues({ name, email, password, vaccinations }) {
+      return {
+          
+        vaccinations: vaccinations || false,
+        name: name || "",
+        email: email || "",
+        password: password || ""
+      };
+    },
+  
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required("You silly!!!"),
+      email: Yup.string().required(),
+      password: Yup.string()
+
+    }),
+  
+    handleSubmit(values, { setStatus }) {
+      axios
+        // values is our object with all our data on it.
+        .post("https://reqres.in/api/users/", values)
+        .then(res => {
+          setStatus(res.data);
+        })
+        .catch(err => console.log(err.response));
+    }
+  })(AnimalForm); // currying functions in Javascript
+  // console.log("This is the HOC", FormikAnimalForm)
+  export default FormikUserForm;
